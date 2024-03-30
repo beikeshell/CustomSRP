4f922800a34136808d4553ecac79849a166df2d5
unknown <beikeshell@126.com>
2024-03-30 20:51:36 +0800

    Drawing Many Instanced Meshes

---
+++ Assets/Custom RP/Samples/MeshBall.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class MeshBall : MonoBehaviour
+{
+    private static int baseColorId = Shader.PropertyToID("_BaseColor");
+
+    [SerializeField]
+    public Mesh mesh = default;
+
+    [SerializeField]
+    public Material material = default;
+
+    private Matrix4x4[] matrices = new Matrix4x4[1023];
+    private Vector4[] baseColors = new Vector4[1023];
+
+    private MaterialPropertyBlock block;
+
+
+    private void Awake()
+    {
+        for (var i = 0; i < matrices.Length; i++)
+        {
+            matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, Quaternion.identity, Vector3.one);
+            baseColors[i] = new Vector4(Random.value, Random.value, Random.value, 1f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (block == null)
+        {
+            block = new MaterialPropertyBlock();
+            block.SetVectorArray(baseColorId, baseColors);
+        }
+        Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
+    }
+}
